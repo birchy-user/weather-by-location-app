4ef93f27795930c4bb5de2fcc59c56d5f28fb50e
Roberts Pelle <roberts.pelle@scandiweb.com>
2023-05-18 22:25:05 +0300

    #1 - commented out OpenWeatherMap usage example

--- src/lib/Map.svelte
+++ src/lib/Map.svelte
@@ -1,19 +1,25 @@
 <script>
     import { onMount } from 'svelte';
     import L from 'leaflet';
-    // import 'leaflet-openweathermap';
+    // import 'leaflet-openweathermap';  // Uncomment to see weather layers:
 
     const center = [56.9677, 24.1056];
 
-    onMount(async () => {
-        const API_KEY = import.meta.env.VITE_API_KEY;
+    // Uncomment to see weather layers:
+    // const connectOpenWeatherMapToLeaflet = (map) => {
+    //     const API_KEY = import.meta.env.VITE_API_KEY;
+    //     let clouds = L.OWM.clouds({showLegend: false, opacity: 0.7, appId: API_KEY});
+    //     let city = L.OWM.current({intervall: 15, appId: API_KEY});
+    //     const baseMaps = { "OSM Standard": osm };
+    //     const overlayMaps = { "Clouds": clouds, "Cities": city };
+    //     const layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
+    // };
 
+    onMount(async () => {
         const osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
             attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
         });
-        
-        let clouds = L.OWM.clouds({showLegend: false, opacity: 0.7, appId: API_KEY});
-        let city = L.OWM.current({intervall: 15, appId: API_KEY});
+    
 
         // Initialize the map
         const map = L.map('map', {
@@ -23,9 +29,8 @@
             layers: [osm]
         });
 
-        const baseMaps = { "OSM Standard": osm };
-        const overlayMaps = { "Clouds": clouds, "Cities": city };
-        const layerControl = L.control.layers(baseMaps, overlayMaps).addTo(map);
+        // Uncomment to see weather layers:
+        // connectOpenWeatherMapToLeaflet(map);
     });
 </script>
 
